e3b64b68b22b1733cb983d0a39d6939d9de5533f
Shintaro Ishimoto <shintaro.ishimoto@gmail.com>
2019-12-28 23:12:45 +0900

    Fix testcase parameter.

--- 2_Perceptron/test/test_perceptron.py
+++ 2_Perceptron/test/test_perceptron.py
@@ -13,31 +13,31 @@ def test_and(x, y, expected):
     assert res == expected
 
 @pytest.mark.parametrize("x, y, expected",[
-    (0, 0, 0),
-    (0, 1, 0),
-    (1, 0, 0),
-    (1, 1, 1),
+    (0, 0, 1),
+    (0, 1, 1),
+    (1, 0, 1),
+    (1, 1, 0),
 ])
 def test_nand(x, y, expected):
-    ret = circuit_and(x, y)
+    ret = circuit_nand(x, y)
     assert res == expected
 
 @pytest.mark.parametrize("x, y, expected",[
     (0, 0, 0),
-    (0, 1, 0),
-    (1, 0, 0),
+    (0, 1, 1),
+    (1, 0, 1),
     (1, 1, 1),
 ])
 def test_or(x, y, expected):
-    ret = circuit_and(x, y)
+    ret = circuit_or(x, y)
     assert res == expected
 
 @pytest.mark.parametrize("x, y, expected",[
-    (0, 0, 0),
+    (0, 0, 1),
     (0, 1, 0),
     (1, 0, 0),
-    (1, 1, 1),
+    (1, 1, 0),
 ])
 def test_xor(x, y, expected):
-    ret = circuit_and(x, y)
+    ret = circuit_xor(x, y)
     assert res == expected
