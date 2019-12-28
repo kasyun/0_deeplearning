626314add7b32880ed0a641388fef6e017dfacb8
Shintaro Ishimoto <shintaro.ishimoto@gmail.com>
2019-12-28 23:08:02 +0900

    Add perceptron (capter 2) mock sources and test cases.

---
+++ 2_Perceptron/src/perceptron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def circuit_and(x, y):
+    return 0
+
+def circuit_nand(x, y):
+    return 0
+
+def circuit_or(x, y):
+    return 0
+
+def circuit_xor(x, y):
+    return 0
+
+if __name__ == '__main__':
+    print(circuit_and(0, 0))
---
+++ 2_Perceptron/test/test_perceptron.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from src/perceptron import circuit_and, circuit_nand, circuit_or, circuit_xor
+
+@pytest.mark.parametrize("x, y, expected",[
+    (0, 0, 0),
+    (0, 1, 0),
+    (1, 0, 0),
+    (1, 1, 1),
+])
+def test_and(x, y, expected):
+    ret = circuit_and(x, y)
+    assert res == expected
+
+@pytest.mark.parametrize("x, y, expected",[
+    (0, 0, 0),
+    (0, 1, 0),
+    (1, 0, 0),
+    (1, 1, 1),
+])
+def test_nand(x, y, expected):
+    ret = circuit_and(x, y)
+    assert res == expected
+
+@pytest.mark.parametrize("x, y, expected",[
+    (0, 0, 0),
+    (0, 1, 0),
+    (1, 0, 0),
+    (1, 1, 1),
+])
+def test_or(x, y, expected):
+    ret = circuit_and(x, y)
+    assert res == expected
+
+@pytest.mark.parametrize("x, y, expected",[
+    (0, 0, 0),
+    (0, 1, 0),
+    (1, 0, 0),
+    (1, 1, 1),
+])
+def test_xor(x, y, expected):
+    ret = circuit_and(x, y)
+    assert res == expected
