ac61a518be019a8b52474e6ebff370bf65c40cf6
Shintaro Ishimoto <shintaro.ishimoto@gmail.com>
2019-12-29 00:47:17 +0900

    Update plot step_function and testcase.

--- 3_NeuralNetwork/src/neuralnetwork.py
+++ 3_NeuralNetwork/src/neuralnetwork.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pylab as plt
+import time
 
 def step_function(x):
-    if x > 0:
-        return 1
-    else:
-        return 0
+    print(f"[step_function] {x}")
+    y = x > 0
+    print(f"[step_function] {y}")
+    return y.astype(np.int)
+
+def plot_step_function():
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_function(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1) # y軸の範囲を指定
+    plt.show()
 
 if __name__ == '__main__':
-    main()
+    plot_step_function()
--- 3_NeuralNetwork/test/test_neuralnetwork.py
+++ 3_NeuralNetwork/test/test_neuralnetwork.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pytest
+import numpy as np
 
 import sys
 sys.path.append('/mnt/c/Users/shint/dev/0_deeplearning//3_NeuralNetwork/src')
 from neuralnetwork import step_function
 
 @pytest.mark.parametrize("x, expected",[
-    (-1.0, 0),
-    (-0.1, 0),
-    (+0.1, 1),
-    (+1.0, 1),
+    ([-1.0, 1.0, 2.0], [0, 1, 1]),
 ])
-def test_and(x, expected):
-    res = step_function(x)
-    assert res == expected
+def test_step_fucntion(x, expected):
+    res = step_function(np.array(x))
+    print(res, type(res))
+    assert list(res) == expected
