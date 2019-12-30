e9d5e7d3b307508e6cf1082b5c3dfd71fdf503b7
sishimoto <46738690+sishimoto@users.noreply.github.com>
2019-12-31 00:51:31 +0900

    Update predict mnist function.

--- 3_NeuralNetwork/src/neural_mnist.py
+++ 3_NeuralNetwork/src/neural_mnist.py
@@ -2,6 +2,9 @@ import sys
 import os
 from PIL import Image
 import numpy as np
+from neuralnetwork import sigmoid, softmax
+import pickle
+import datetime
 
 
 def load_dataset():
@@ -14,6 +17,43 @@ def load_dataset():
     )
 
     return (x_train, t_train), (x_test, t_test)
+
+
+def get_data():
+    return load_dataset()[1]
+
+
+def init_network():
+    with open("../../deep-learning-from-scratch/ch03/sample_weight.pkl", "rb") as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+
+def calc_mnist():
+    x, t = get_data()
+    network = init_network()
+
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network, x[i])
+        p = np.argmax(y)
+        if p == t[i]:
+            accuracy_cnt += 1
+    print(f"Accuracy: {str(float(accuracy_cnt) / len(x))}")
 
 
 def print_mnist():
@@ -45,5 +85,9 @@ def plot_mnist():
 
 
 if __name__ == '__main__':
+    start = datetime.datetime.now()
     # print_mnist()
-    plot_mnist()
+    # plot_mnist()
+    calc_mnist()
+    end = datetime.datetime.now()
+    print(f"elapsed time = {end - start}")
